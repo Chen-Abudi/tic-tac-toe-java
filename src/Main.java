0e105fb1899c51a5c85d5a7beb1165b65d882516
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-18 17:38:37 +0300

    feat: Add positions to the game

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("   |   |   ");
-        System.out.println("-----------");
-        System.out.println("   |   |   ");
-        System.out.println("-----------");
-        System.out.println("   |   |   ");
+        char[] positions = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+
+        Scanner scanner = new Scanner(System.in);
+        int input;
+
+        while (true) {
+            System.out.print("Enter a position: ");
+
+            input = scanner.nextInt();
+            positions[input - 1] = 'X';
+
+            System.out.println(" " + positions[6] + " | " + positions[7] + " | " + positions[8] +  " ");
+            System.out.println("---+---+---");
+            System.out.println(" " + positions[3] + " | " + positions[4] + " | " + positions[5] +  " ");
+            System.out.println("---+---+---");
+            System.out.println(" " + positions[0] + " | " + positions[1] + " | " + positions[2] +  " ");
+        }
     }
 }

608b5c75e276b3f7216a76af7b5ddfe7c8e36c73
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-18 23:48:41 +0300

    feat: Add win condition checks and turn switch

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,41 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         int input;
+        char turn = 'X';
 
         while (true) {
-            System.out.print("Enter a position: ");
+            do {
+                System.out.print("Enter a position: ");
 
-            input = scanner.nextInt();
-            positions[input - 1] = 'X';
+                input = scanner.nextInt();
+            } while (positions[input - 1] == 'X' || positions[input - 1] == 'O');
+
+            positions[input - 1] = turn;
 
             System.out.println(" " + positions[6] + " | " + positions[7] + " | " + positions[8] +  " ");
             System.out.println("---+---+---");
             System.out.println(" " + positions[3] + " | " + positions[4] + " | " + positions[5] +  " ");
             System.out.println("---+---+---");
             System.out.println(" " + positions[0] + " | " + positions[1] + " | " + positions[2] +  " ");
+
+            if (positions[0] == turn && positions[1] == turn && positions[2] == turn
+                    || positions[3] == turn && positions[4] == turn && positions[5] == turn
+                    || positions[6] == turn && positions[7] == turn && positions[8] == turn
+                    || positions[6] == turn && positions[3] == turn && positions[0] == turn
+                    || positions[7] == turn && positions[4] == turn && positions[1] == turn
+                    || positions[8] == turn && positions[5] == turn && positions[2] == turn
+                    || positions[6] == turn && positions[4] == turn && positions[2] == turn
+                    || positions[8] == turn && positions[4] == turn && positions[0] == turn) {
+                System.out.println(turn + " is the WINNER");
+                break;
+            }
+
+            if (turn == 'X') {
+                turn = 'O';
+            }
+            else if (turn == 'O') {
+                turn = 'X';
+            }
         }
     }
 }
